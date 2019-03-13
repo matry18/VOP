d12cb2b1bd77b14360a40d3a0fb070ac8f19b5cc
Mathias Tryggedsson <43138771+matry18@users.noreply.github.com>
2019-03-13 08:17:45 +0100

    NetworkClientServer

---
+++ src/SocketClient/SocketClient.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SocketClient;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author aalsc
+ */
+public class SocketClient {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String s;
+        Scanner inputStream = null;
+        PrintWriter outputStream = null;
+        
+        try {
+            //Connect to server on same machine port 6789
+            Socket clientSocket = new Socket("localhost", 6789);
+            //Set up stream to send/recieve data
+            inputStream = new Scanner(new InputStreamReader(clientSocket.getInputStream()));
+            outputStream = new PrintWriter(new DataOutputStream(clientSocket.getOutputStream()));
+            
+            //Send "Java" to server
+            outputStream.println("Java");
+            outputStream.flush(); //Sends data to server
+            
+            //Read anything from the server until nomore lines and output it to the screen
+            while (inputStream.hasNext()) {
+                s = inputStream.nextLine();
+                System.out.println(s);
+            }
+            inputStream.close();
+            outputStream.close();
+            
+        } catch (IOException ex) {
+            //If any exception occurs display it
+            System.out.println("Error"+ex);
+        }
+        
+        
+        
+    }
+    
+}
