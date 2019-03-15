6cabb986c9bed8670f9e2cbec9128dd7fec687b3
Mathias Tryggedsson <43138771+matry18@users.noreply.github.com>
2019-03-15 08:16:38 +0100

    RBToggleGroup

--- src/opg3_playingcards/Card.java
+++ src/opg3_playingcards/Card.java
@@ -1,11 +1,66 @@
 package opg3_playingcards;
 
 /**
- * VOP eksamen F2014
- * Tom klasse til opgave 3
+ * VOP eksamen F2014 Tom klasse til opgave 3
+ *
  * @author erso
  */
 public class Card implements CardInterface {
 
+    private int face;
+    private int suit;
 
+    public Card(int face, int suit)  {  
+
+        if (face < ACE || face > KING || suit < CLUBS || suit > SPADES) {
+            System.out.println("Ulovligt kort: " + suit + " " + face);
+            return;
+        }
+        this.face = face;
+        this.suit = suit;
+    }
+    public String toString() {
+        String cardName = "";
+        switch (suit) {
+            case CLUBS:
+                cardName += CLUBS_NAME;
+                break;
+            case DIAMONDS:
+                cardName += DIAMONDS_NAME;
+                break;
+            case HEARTS:
+                cardName += HEARTS_NAME;
+                break;
+            case SPADES:
+                cardName += SPADES_NAME;
+                break;
+        }
+        switch (face) {
+            case ACE:
+                cardName += ACE_NAME;
+                break;
+            case JACK:
+                cardName += JACK_NAME;
+                break;
+            case QUEEN:
+                cardName += QUEEN_NAME;
+                break;
+            case KING:
+                cardName += KING_NAME;
+                break;
+        }
+        return cardName;
+    }
+public static void main(String[] args) {
+       
+            System.out.println(new Card(1, 3));
+            System.out.println(new Card(3, 1));
+            System.out.println(new Card(13, 4));
+            System.out.println(new Card(12, 3));
+            System.out.println(new Card(17, 5));
+            
+
+
+
+    }
 }
--- src/opg3_playingcards/DeckOfCards.java
+++ src/opg3_playingcards/DeckOfCards.java
@@ -1,5 +1,7 @@
 package opg3_playingcards;
 
+import java.util.Random;
+
 /**
  * VOP eksamen F2014
  * Kodeskelet til opgave 3c og 3d
@@ -11,7 +13,14 @@ public class DeckOfCards implements CardInterface {
 
     public DeckOfCards(){
         // Opg 3c. Initialiser deck, dan de 52 lovlige kort og saet dem i deck-arrayet
-        
+        deck = new Card[NUMBER_OF_CARDS];
+        int i = 0;
+        for (int v = ACE; v <= KING; v++) {
+            for (int c = CLUBS; c <= SPADES; c++) {
+                deck[i] = new Card(v,c);
+                i++;
+            }
+        }
     }
 
     // Faerdiskrevet metode til "paen" udskrift af kortbunken
@@ -32,6 +41,17 @@ public class DeckOfCards implements CardInterface {
 
     public void shuffle(int swaps) {
         // Opgave 3d) Bland kortene
+        Random gen = new Random();
+        Card temp = null;
+        int position1, position2;
+        for (int i = 0; i < swaps; i++) {
+            position1 = gen.nextInt(deck.length);
+            position2 = gen.nextInt(deck.length);
+            
+            temp = deck[position1];
+            deck[position1] = deck[position2];
+            deck[position2] = temp;
+        }
     }
 
    
