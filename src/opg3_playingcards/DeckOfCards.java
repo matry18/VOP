1a48d6368a93c4086f1813c4b927dfe22a00b24c
Mathias Tryggedsson <43138771+matry18@users.noreply.github.com>
2019-03-15 11:16:11 +0100

    UpdateIgnore

--- src/opg3_playingcards/DeckOfCards.java
+++ src/opg3_playingcards/DeckOfCards.java
@@ -1,7 +1,5 @@
 package opg3_playingcards;
 
-import java.util.Random;
-
 /**
  * VOP eksamen F2014
  * Kodeskelet til opgave 3c og 3d
@@ -13,14 +11,7 @@ public class DeckOfCards implements CardInterface {
 
     public DeckOfCards(){
         // Opg 3c. Initialiser deck, dan de 52 lovlige kort og saet dem i deck-arrayet
-        deck = new Card[NUMBER_OF_CARDS];
-        int i = 0;
-        for (int v = ACE; v <= KING; v++) {
-            for (int c = CLUBS; c <= SPADES; c++) {
-                deck[i] = new Card(v,c);
-                i++;
-            }
-        }
+        
     }
 
     // Faerdiskrevet metode til "paen" udskrift af kortbunken
@@ -41,17 +32,6 @@ public class DeckOfCards implements CardInterface {
 
     public void shuffle(int swaps) {
         // Opgave 3d) Bland kortene
-        Random gen = new Random();
-        Card temp = null;
-        int position1, position2;
-        for (int i = 0; i < swaps; i++) {
-            position1 = gen.nextInt(deck.length);
-            position2 = gen.nextInt(deck.length);
-            
-            temp = deck[position1];
-            deck[position1] = deck[position2];
-            deck[position2] = temp;
-        }
     }
 
    
