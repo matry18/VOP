e763600756276bbc7cc3b884d39830584418207a
Mathias Tryggedsson <43138771+matry18@users.noreply.github.com>
2019-05-18 18:45:34 +0200

    opdateret

---
+++ src/Lektion_07_Call_Back_Interface_GUI/FXMLDocumentController.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lektion_07_Call_Back_Interface_GUI;
+
+import Lektion_07_Call_Back_Interface_Facade.CallBackInterface;
+import Lektion_07_Call_Back_Interface_Facade.FacadeWithCallback;
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+/**
+ *
+ * @author erso
+ */
+public class FXMLDocumentController implements Initializable, CallBackInterface {
+
+    @FXML
+    private TextArea textArea;
+    @FXML
+    private Button startButton;
+    @FXML
+    private Button stopButton;
+    @FXML
+    private ImageView die1view;
+    @FXML
+    private ImageView die2view;
+
+    private Thread facade;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        startButton.setDisable(false);
+        stopButton.setDisable(true);
+    }
+
+    @FXML
+    private void buttonAction(ActionEvent event) {
+        if (event.getSource() == startButton) {
+            facade = new FacadeWithCallback(this);
+            facade.start();
+            startButton.setDisable(true);
+            stopButton.setDisable(false);
+        } else {
+            facade.interrupt();
+            startButton.setDisable(false);
+            stopButton.setDisable(true);
+        }
+    }
+
+    @Override
+    public void updateMessage(String message) {
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                textArea.appendText(message + "\n");
+                if(!facade.isAlive()){
+                    stopButton.fire();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void updateImages(File i1, File i2) {
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                die1view.setImage(new Image(i1.toURI().toString()));
+                die2view.setImage(new Image(i2.toURI().toString()));
+            }
+        });
+    }
+
+}
